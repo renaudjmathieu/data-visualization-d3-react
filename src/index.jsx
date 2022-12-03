860495a349afd42fafdad879bd014f609c5f6c70
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-12-03 02:46:47 -0500

    wattenberger template

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { mainNavbarListItems } from './components/Navbar/consts/navbarListItems'
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {mainNavbarListItems.map((item, index) => (
-            <Route
-              path={item.path}
-              element={item.element} />
-          ))}
-          <Route path="*" element={<h1>404: Not Found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
