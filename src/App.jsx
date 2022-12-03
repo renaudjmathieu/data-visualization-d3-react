f28b76b9dd2c03df0a5dee360e0a99929231465a
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-12-03 05:05:59 -0500

    dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,17 @@
-import React, { useState, useEffect, useRef } from "react"
-import * as d3 from "d3"
-import { getTimelineData, getScatterData } from "./utils/dummyData"
-
-import Timeline from "./components/Timeline"
-import ScatterPlot from "./components/ScatterPlot"
-import Histogram from "./components/Histogram"
+import React from "react"
+import Dashboard from "./components/Dashboard"
 
 import "./styles.css"
 
-const parseDate = d3.timeParse("%m/%d/%Y")
-const dateAccessor = d => parseDate(d.date)
-const temperatureAccessor = d => d.temperature
-const humidityAccessor = d => d.humidity
-
-const getData = () => ({
-  timeline: getTimelineData(),
-  scatter: getScatterData(),
-})
 const App = () => {
-  const [data, setData] = useState(getData())
-
-  useInterval(() => {
-    setData(getData())
-  }, 4000)
-
-  return (
-    <div className="App">
-      <h1>
-        Weather Dashboard
-      </h1>
-      <div className="App__charts">
-        <Timeline
-          data={data.timeline}
-          xAccessor={dateAccessor}
-          yAccessor={temperatureAccessor}
-          label="Temperature"
-        />
-        <ScatterPlot
-          data={data.scatter}
-          xAccessor={humidityAccessor}
-          yAccessor={temperatureAccessor}
-          xLabel="Humidity"
-          yLabel="Temperature"
-        />
-        <Histogram
-          data={data.scatter}
-          xAccessor={humidityAccessor}
-          label="Humidity"
-        />
-      </div>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>
+                Weather Dashboard
+            </h1>
+            <Dashboard />
+        </div>
+    )
 }
 
 export default App
-
-
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
