04a6b7813f7cda12f42f96ac95a0dbfc56174fca
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-12-03 07:19:29 -0500

    ajouter navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,242 @@
-import React from "react"
+import * as React from 'react';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { grey } from '@mui/material/colors';
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
 import Dashboard from "./components/Dashboard"
 
 import "./styles.css"
 
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginRight: -drawerWidth,
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginRight: 0,
+        }),
+    }),
+);
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: drawerWidth,
+    }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+}));
+
 const App = () => {
+    const storedDarkMode = localStorage.getItem("DARK_MODE");
+    const [mode, setMode] = React.useState(storedDarkMode ?? "light");
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+            },
+        }),
+        []
+    );
+
+    // Whenever dark mode changes, update the localStorage DARK_MODE item
+    React.useEffect(() => {
+        localStorage.setItem("DARK_MODE", mode);
+    }, [mode]);
+
+    // Whenever the mode changes, change the theme 
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                    primary: {
+                        ...(mode !== "light"
+                            ? {
+                                light: "#455a64",
+                                main: "#464B50",
+                                dark: "#263238",
+                                contrastText: "#eceff1",
+                            }
+                            : {
+                                light: "#455a64",
+                                main: "#161C24",
+                                dark: "black",
+                                contrastText: "#eceff1",
+                            }),
+                    },
+                    secondary: {
+                        ...(mode === "light"
+                            ? {
+                                main: '#161C24',
+                            }
+                            : {
+                                main: '#29434e',
+                            })
+                    },
+                    text: {
+                        ...(mode === "light"
+                            ? {
+                                primary: grey[800],
+                                secondary: grey[300],
+                                disabled: grey[500],
+                            }
+                            : {
+                                primary: grey[200],
+                                secondary: grey[300],
+                                disabled: grey[500],
+                            }),
+                    },
+                    background: {
+                        ...(mode === "light"
+                            ? {
+                                paper: "#161C24",
+                                default: "#eceff1",
+                                neutral: "#161C24",
+                            }
+                            : {
+                                paper: "#161C24",
+                                default: "#161C24",
+                                neutral: "black",
+                            }),
+                    },
+                },
+            }),
+        [mode]
+    );
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div className="App">
-            <h1>
-                Weather Dashboard
-            </h1>
-            <Dashboard />
-        </div>
-    )
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="App">
+                    <AppBar position="fixed" open={open}>
+                        <Toolbar>
+                            <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} component="div">
+                                Weather Dashboard
+                            </Typography>
+                            <IconButton
+                                sx={{ ml: 1 }}
+                                onClick={colorMode.toggleColorMode}
+                                color="inherit"
+                            >
+                                {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+                            </IconButton>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                edge="end"
+                                onClick={handleDrawerOpen}
+                                sx={{ ...(open && { display: 'none' }) }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                        </Toolbar>
+                    </AppBar>
+                    <Main open={open}>
+                        <DrawerHeader />
+                        <Dashboard />
+                    </Main>
+                    <Drawer
+                        sx={{
+                            width: drawerWidth,
+                            flexShrink: 0,
+                            '& .MuiDrawer-paper': {
+                                width: drawerWidth,
+                            },
+                        }}
+                        variant="persistent"
+                        anchor="right"
+                        open={open}
+                    >
+                        <DrawerHeader>
+                            <IconButton onClick={handleDrawerClose}>
+                                {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                            </IconButton>
+                        </DrawerHeader>
+                        <Divider />
+                        <List>
+                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Drawer>
+                </div>
+            </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
