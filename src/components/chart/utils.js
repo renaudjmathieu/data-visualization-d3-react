522c79d2676be0f073fd20c68aba261ef2b5c1c1
Renaud Mathieu <renaudjmathieu@gmail.com>
2022-12-03 15:20:10 -0500

    turn off charts transitions on resize

--- src/components/chart/utils.js
+++ src/components/chart/utils.js
@@ -3,80 +3,89 @@ import { useEffect, useState, useRef } from "react"
 import ResizeObserver from "resize-observer-polyfill"
 
 export const accessorPropsType = (
-  PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.number,
-  ])
+    PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.number,
+    ])
 )
 
 export const callAccessor = (accessor, d, i) => (
-  typeof accessor === "function" ? accessor(d, i) : accessor
+    typeof accessor === "function" ? accessor(d, i) : accessor
 )
 
 export const dimensionsPropsType = (
-  PropTypes.shape({
-    height: PropTypes.number,
-    width: PropTypes.number,
-    marginTop: PropTypes.number,
-    marginRight: PropTypes.number,
-    marginBottom: PropTypes.number,
-    marginLeft: PropTypes.number,
-  })
+    PropTypes.shape({
+        height: PropTypes.number,
+        width: PropTypes.number,
+        marginTop: PropTypes.number,
+        marginRight: PropTypes.number,
+        marginBottom: PropTypes.number,
+        marginLeft: PropTypes.number,
+    })
 )
 
 export const combineChartDimensions = dimensions => {
-  let parsedDimensions = {
-    marginTop: 40,
-    marginRight: 30,
-    marginBottom: 40,
-    marginLeft: 75,
-    ...dimensions,
-  }
+    let parsedDimensions = {
+        marginTop: 40,
+        marginRight: 30,
+        marginBottom: 40,
+        marginLeft: 75,
+        ...dimensions,
+    }
 
-  return {
-    ...parsedDimensions,
-    boundedHeight: Math.max(parsedDimensions.height - parsedDimensions.marginTop - parsedDimensions.marginBottom, 0),
-    boundedWidth: Math.max(parsedDimensions.width - parsedDimensions.marginLeft - parsedDimensions.marginRight, 0),
-  }
+    return {
+        ...parsedDimensions,
+        boundedHeight: Math.max(parsedDimensions.height - parsedDimensions.marginTop - parsedDimensions.marginBottom, 0),
+        boundedWidth: Math.max(parsedDimensions.width - parsedDimensions.marginLeft - parsedDimensions.marginRight, 0),
+    }
 }
 
 export const useChartDimensions = passedSettings => {
-  const ref = useRef()
-  const dimensions = combineChartDimensions(passedSettings)
+    const ref = useRef()
+    const dimensions = combineChartDimensions(passedSettings)
 
-  const [width, changeWidth] = useState(0)
-  const [height, changeHeight] = useState(0)
+    const [width, changeWidth] = useState(0)
+    const [height, changeHeight] = useState(0)
 
-  useEffect(() => {
-    if (dimensions.width && dimensions.height) return [ref, dimensions]
+    useEffect(() => {
+        if (dimensions.width && dimensions.height) return [ref, dimensions]
 
-    const element = ref.current
-    const resizeObserver = new ResizeObserver(entries => {
-      if (!Array.isArray(entries)) return
-      if (!entries.length) return
+        const element = ref.current
+        const resizeObserver = new ResizeObserver(entries => {
+            if (!Array.isArray(entries)) return
+            if (!entries.length) return
 
-      const entry = entries[0]
+            const entry = entries[0]
 
-      if (width !== entry.contentRect.width) changeWidth(entry.contentRect.width)
-      if (height !== entry.contentRect.height) changeHeight(entry.contentRect.height)
+            let resizeTimer;
+            if (width !== entry.contentRect.width || height !== entry.contentRect.height) {
+                document.body.classList.add("resize-animation-stopper");
+                clearTimeout(resizeTimer);
+                resizeTimer = setTimeout(() => {
+                    document.body.classList.remove("resize-animation-stopper");
+                }, 300);
+            }
+
+            if (width !== entry.contentRect.width) changeWidth(entry.contentRect.width)
+            if (height !== entry.contentRect.height) changeHeight(entry.contentRect.height)
+        })
+
+        resizeObserver.observe(element)
+
+        return () => resizeObserver.unobserve(element)
+    }, [passedSettings, height, width, dimensions])
+
+    const newSettings = combineChartDimensions({
+        ...dimensions,
+        width: dimensions.width || width,
+        height: dimensions.height || height,
     })
 
-    resizeObserver.observe(element)
-
-    return () => resizeObserver.unobserve(element)
-  }, [passedSettings, height, width, dimensions])
-
-  const newSettings = combineChartDimensions({
-    ...dimensions,
-    width: dimensions.width || width,
-    height: dimensions.height || height,
-  })
-
-  return [ref, newSettings]
+    return [ref, newSettings]
 }
 
 let lastId = 0
-export const useUniqueId = (prefix="") => {
-  lastId++
-  return [prefix, lastId].join("-")
+export const useUniqueId = (prefix = "") => {
+    lastId++
+    return [prefix, lastId].join("-")
 }
